2ee51ff81ebf8ec7e562048e2384686d73ba1aa6
Charlotte Thouvenin <charlotte_thouvenin@hotmail.fr>
2023-11-14 14:31:10 +0100

    Update fonctions.js

--- FrontEnd/assets/script/fonctions.js
+++ FrontEnd/assets/script/fonctions.js
@@ -1,56 +1,57 @@
-// fonction fetch
 let utilData = []
 
 let categoriesArray = []
 
 let boutonsArray = []
 
+
+// fonction fetch
 async function fetchData() {
     const response = await fetch("http://localhost:5678/api/works");
     const data = await response.json();
     console.log(data)
 
 
-//récupérer uniquement les propriétés utiles
+    //récupérer uniquement les propriétés utiles
 
     utilData = data.map(item => ({
-      imageUrl: item.imageUrl,
-      title: item.title,
-      categoryName: item.category.name
+        imageUrl: item.imageUrl,
+        title: item.title,
+        categoryName: item.category.name
     }));
     console.log(utilData)
 
 
-//récupérer les catégories
+    //récupérer les catégories
 
-const categoriesSet = new Set();
+    const categoriesSet = new Set();
 
-utilData.forEach(item => {
-  categoriesSet.add(item.categoryName);
-});
-categoriesArray = Array.from(categoriesSet);
-//rajout de la catégorie "tous"
-categoriesArray.unshift("Tous");
+    utilData.forEach(item => {
+        categoriesSet.add(item.categoryName);
+    });
+    categoriesArray = Array.from(categoriesSet);
+    //rajout de la catégorie "tous"
+    categoriesArray.unshift("Tous");
 
-console.log(categoriesArray);
+    console.log(categoriesArray);
 
 
-//
+    //
     genererGallery(utilData);
 
     genererFiltres(categoriesArray);
 
-}  
+}
 
 
 
 
 // fonction generer galerie
 
-function genererGallery(data){
-    for (let i = 0; i<data.length; i++){
+function genererGallery(data) {
+    for (let i = 0; i < data.length; i++) {
         const gallery = document.querySelector(".gallery");
-        
+
         const work = data[i];
 
         //création d'une balise qui contient un travail
@@ -64,11 +65,11 @@ function genererGallery(data){
         workImage.alt = work.title;
 
         //creation du titre
-        const workFigCaption = document.createElement ("figcaption");
+        const workFigCaption = document.createElement("figcaption");
         workFigure.appendChild(workFigCaption);
         workFigCaption.innerText = work.title;
 
-        
+
     }
 
 }
@@ -101,37 +102,32 @@ function filtrer(data, category) {
     console.log(data);
     console.log(category);
     return data.filter(item => item.categoryName === category);
-  }
-
-// fonction generer works filtrés
-
+}
 
 
 
 // application des filtres
 
-function listenerFiltres(){
+function listenerFiltres() {
     const boutonWorkFiltres = document.querySelectorAll(".filtres button");
     console.log(boutonWorkFiltres)
 
-    for(let i = 0; i<boutonWorkFiltres.length; i++){
+    for (let i = 0; i < boutonWorkFiltres.length; i++) {
         boutonWorkFiltres[i].addEventListener("click", function () {
             console.log(boutonWorkFiltres[i].categoryName)
             console.log(utilData)
-            if (i>0) {
-                  let workFiltres = filtrer (utilData, boutonWorkFiltres[i].categoryName)
-            console.log(workFiltres)
+            if (i > 0) {
+                let workFiltres = filtrer(utilData, boutonWorkFiltres[i].categoryName)
+                console.log(workFiltres)
 
-            document.querySelector(".gallery").innerHTML = "";
-            genererGallery(workFiltres);
-            }
-            else {
+                document.querySelector(".gallery").innerHTML = "";
+                genererGallery(workFiltres);
+            } else {
                 document.querySelector(".gallery").innerHTML = "";
                 genererGallery(utilData);
             }
 
-            })
-            
-        } 
-    }
+        })
 
+    }
+}
