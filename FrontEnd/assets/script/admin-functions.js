b42c81e647258c29482e07b08f0e0715211e4f75
Charlotte Thouvenin <charlotte_thouvenin@hotmail.fr>
2023-11-16 13:36:46 +0100

    login ok

---
+++ FrontEnd/assets/script/admin-functions.js
@@ -0,0 +1,9 @@
+// transofrmer le login en logout
+
+// listener sur logout
+
+// modifier la page au logout
+
+// ajout bouton modifier
+
+// création modale
--- FrontEnd/assets/script/login-functions.js
+++ FrontEnd/assets/script/login-functions.js
@@ -1,24 +1,67 @@
 // creation de l'objet d'authentification au submit
 
-function loginData() {
+function loginDataListener() {
     const loginForm = document.querySelector(".login__form");
-    console.log(loginForm);
+    let loginData = []
 
     loginForm.addEventListener("submit", function (event) {
+
         event.preventDefault();
 
-        // Utilisez # pour sélectionner l'élément par ID et .value pour récupérer la valeur
-      
-
-        const loginData = {
+        loginData = {
             email: event.target.querySelector("#email").value,
             password: event.target.querySelector("#mdp").value
         };
 
-        console.log(loginData);
+        const chargeUtileData = chargeUtile(loginData);
+
+        sendLoginData(chargeUtileData)
+
     });
+
+}
+// fonction convertir en json
+function chargeUtile(data) {
+    const chargeUtileData = JSON.stringify(data)
+    return chargeUtileData
 }
 
 // fonction envoi donner authentification
+async function sendLoginData(chargeUtile) {
+    try{
+        const postStatut = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: chargeUtile
+        })
 
-//
+        if (!postStatut.ok) {
+            throw new Error(`${postStatut.status}: ${errorMessage}`);
+        }
+
+        const response = await postStatut.json()
+       
+        logInOk(response)
+
+    } catch (error) {
+        messageErreur()
+    }
+}
+
+
+// fonction afficher message erreur
+
+function messageErreur() {
+    const messageErreurElement = document.querySelector(".messageErreur");
+    messageErreurElement.innerText = "Utilisateur non trouvé, veuillez réessayer";
+}
+
+
+
+
+// fonction renvoyer sur page d'acceuil et stocker le token
+
+function logInOk(response){
+    sessionStorage.setItem("Token", response.token);
+    window.location.replace("index.html")
+}
